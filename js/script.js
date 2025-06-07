f45b69f8643acf1be2e4b4b3856b93368f3bb246
Dan Kirby <git-fraim@smibbs.cc>
2025-06-07 18:33:29 +0100

    Task 2.2: Load Questions Data

--- js/script.js
+++ js/script.js
@@ -1,2 +1,10 @@
 console.log("JS loaded");
-console.log("First question:", questions[0]); 
+
+// Verify questions data is loaded
+if (typeof questions === 'undefined') {
+    console.error('Questions data failed to load');
+} else {
+    console.log('Questions data loaded successfully');
+    console.log(`Loaded ${questions.length} questions`);
+    console.log('First question:', questions[0]);
+} 
