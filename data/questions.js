0aadbf768dfd02396bc5cbc7ca6ed1ecff65d309
Dan Kirby <git-fraim@smibbs.cc>
2025-06-07 18:31:53 +0100

    Task 2.1: Create Questions Data Structure

---
+++ data/questions.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        id: 1,
+        question: "What is JavaScript?",
+        answer: "JavaScript is a high-level, interpreted programming language primarily used for creating interactive web applications."
+    },
+    {
+        id: 2,
+        question: "What is the DOM?",
+        answer: "The Document Object Model (DOM) is a programming interface for HTML documents that represents the page as a tree-like structure of objects."
+    },
+    {
+        id: 3,
+        question: "What is CSS?",
+        answer: "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in HTML."
+    }
+];
+
+// Make questions array globally accessible
+window.questions = questions; 
--- js/script.js
+++ js/script.js
@@ -1,1 +1,2 @@
-console.log("JS loaded"); 
+console.log("JS loaded");
+console.log("First question:", questions[0]); 
